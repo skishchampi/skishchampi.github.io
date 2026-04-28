458f7bf68f5e4cbfbf819928271c36fa56ae64e6
skishchampi <skishchampi@users.noreply.github.com>
2026-04-27 21:56:13 -0400

    Unify tag URLs and add direct contact email link

--- _plugins/tags.rb
+++ _plugins/tags.rb
@@ -5,21 +5,23 @@ module Jekyll
     def generate(site)
       tags = site.posts.docs.flat_map { |post| post.data['tags'] || [] }.to_set
       tags.each do |tag|
-        site.pages << TagPage.new(site, site.source, tag)
+        tag_slug = Jekyll::Utils.slugify(tag.to_s, mode: "pretty", cased: false)
+        site.pages << TagPage.new(site, site.source, tag, tag_slug)
       end
     end
   end
 
   class TagPage < Page
-    def initialize(site, base, tag)
+    def initialize(site, base, tag, tag_slug)
       @site = site
       @base = base
-      @dir  = File.join('tag', tag)
+      @dir  = File.join('tags', tag_slug)
       @name = 'index.html'
 
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'tag.html')
       self.data['tag'] = tag
+      self.data['tag_slug'] = tag_slug
       self.data['title'] = "Tag: #{tag}"
     end
   end
